f0c905fe9168181ede4443f5b1360657abfae1b7
Paul Kallarackel <paulkallarackel@gmail.com>
2023-03-24 22:16:43 +0000

    Hello World

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      <h1>Hello World!</h1>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +28,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </div>
   )
 }
