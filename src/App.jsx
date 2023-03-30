731a53fff55b375da4c4eeb66644c8671f868752
Paul Kallarackel <paulkallarackel@gmail.com>
2023-03-30 14:08:23 +0100

    newer version

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      {/* <div>
+      <h1>Hello World!</h1> 
+       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,7 +28,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p> */}
+      </p>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 
-const myFirstElement = <h1>Hello React!</h1>
+// const myFirstElement = <h1>Hello React!</h1>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(myFirstElement);
